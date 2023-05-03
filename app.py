e74edc471b1714bb80c89276ff506ecd10525487
silverVern <98193911+silverVern@users.noreply.github.com>
2023-05-03 17:36:43 +1200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, jsonify, render_template
+import pandas as pd
+
+import joblib
+from utils import preprocessor
+
+app = Flask(__name__)
+model = joblib.load(r'C:\Users\verno\IOD\02 - Labs\Module 10\IOD_Lab-10_2\model.joblib')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+
+    input = request.form['text']
+    predicted_sentiment = model.predict(pd.Series(input))[0]
+    if predicted_sentiment == 1:
+        output = 'positive'
+    else:
+        output = 'negative'
+
+    return render_template('index.html', sentiment=f'Predicted sentiment of "{input}" is {output}.')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
