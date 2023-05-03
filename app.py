ce92f120bb654743ba3a18ff885a2bf787d7ac56
silverVern <98193911+silverVern@users.noreply.github.com>
2023-05-03 19:10:07 +1200

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import joblib
 from utils import preprocessor
 
 app = Flask(__name__)
-model = joblib.load(r'C:\Users\verno\IOD\02 - Labs\Module 10\IOD_Lab-10_2\model.joblib')
+model = joblib.load(r'model.joblib')
 
 @app.route('/')
 def home():
